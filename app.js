dff6cf26ed77b2fd5df6c20a1aee7ac1257b79d5
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-08-23 23:47:31 +0500

    minor bug fixes

--- app.js
+++ app.js
@@ -10,11 +10,26 @@ const config = require('./config')
 const app = express()
 
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({ origin: 'https://anki-backend.vercel.app/' }))
 app.use(express.json())
 app.use('/api/auth', authRoutes)
 app.use('/api/cards', cardRoutes)
 app.use('/api/todos', todoRoutes)
+app.post('/api/token/refresh', (req, res) => {
+	const { refreshToken } = req.body
+	if (!refreshToken) return res.status(400).send('Refresh token is required')
+
+	jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, user) => {
+		if (err) return res.status(403).send('Invalid refresh token')
+
+		const accessToken = jwt.sign(
+			{ userId: user.userId },
+			process.env.JWT_ACCESS_SECRET,
+			{ expiresIn: '15m' }
+		)
+		res.json({ accessToken })
+	})
+})
 
 mongoose
 	.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
