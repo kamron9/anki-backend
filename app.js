d2065f3134394962055eb973a7f749518f6be310
Alimov Kamronbek <kamronalimov80@gmail.com>
2024-08-23 23:06:26 +0500

    minor bug fixes

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use(cors())
-
+app.use(express.json())
 app.use('/api/auth', authRoutes)
 app.use('/api/cards', cardRoutes)
 app.use('/api/todos', todoRoutes)
 
-mongoose.connect(config.mongoURI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
+mongoose
+	.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log('MongoDBga muvaffaqiyatli ulandi'))
+	.catch(error => console.error('MongoDBga ulanishda xatolik: ', error))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
